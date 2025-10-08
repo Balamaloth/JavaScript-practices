8846a4a030e5b6ffadb4ce62e3d1cae81aaaf122
Balamaloth <168509785+Balamaloth@users.noreply.github.com>
2025-10-08 23:39:51 +0530

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,34 @@
+/* 8 types of variables 
+bbssnnou
+ 1 bigint 
+ 2 boolean
+ 3 sting 
+ 4 symboles
+ 5 number 
+ 6 null 
+ 7 object
+ 8 underfined  */           
+                               /* types of primitive and non primitive  
+                               primitive data types are pre defined in the language  and cannot be changed 
+                               non primitive or reference type can be changed and modified
+                                 */                   
+                                                       /* how many ways java script code written 
+                                                             1. inline
+                                                             2. internal 
+                                                             3. external 
+                                                             
+                                                             inline means writing the code in single line 
+                                                             internal means writing the code in <script> tag
+                                                             external means writing the code in separate file and linking it to html file */
+                                                             
+
+/* scole types
+gbole 1 
+local 2
+block 3 
+gbole scope means 
+ the variable can be accessed from any where in the program 
+ local scope means
+ the variable can be accessed oonly within the function 
+ block scope means 
+ the variable can be accessed only within the block of code  */
