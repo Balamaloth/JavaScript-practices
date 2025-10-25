73e38677e829f3a00c9cbe0011787226f0329faf
Ybala maloth <balasainik3@gmail.com>
2025-10-25 19:10:57 +0530

    First commit

---
+++ what is js/js.js
@@ -0,0 +1,42 @@
+//   what is js
+   // JavaScript was created by Brendan Eich in 1995
+   // It was originally called Mocha, then LiveScript, and finally JavaScript
+
+
+// HTML → defines structure/content
+// CSS → defines style/design
+// JavaScript → defines behavior/interaction
+
+
+// javascript is a high level , interpreted , dynamically typed , object - based , event-driven , and multi-paradigm program language primarily used to make web pages interactive 
+
+// - high - level language 
+// JavaScript is high-level, meaning it is easy for humans to read and write
+ // You don’t have to manage memory or hardware details — the browser ( Node.js) handles that for you
+
+ // Interpreted Language ==
+ //  line -by -  line interpreted 
+ // JavaScript code is not compiled before running (like C or Java)
+ // You can run JS directly in a browser
+ //  You don’t need a separate compiler
+
+ // Dynamically Typed
+ // You don’t need to declare data types for variables
+// The type is decided at runtime (while the program runs)
+
+// Object-Based / Object-Oriented
+//  Everything in JavaScript (like arrays, functions, objects) can act like an object
+// You can create and use objects easily
+
+
+// Event-Driven and Asynchronous
+// JavaScript can respond to events (like clicks or keypresses)
+// It can handle multiple tasks (using callbacks, promises, async/await) without blocking the program
+
+
+// Multi-Paradigm
+//  JavaScript supports many styles of programming
+// Procedural (step-by-step)
+// Object-Oriented
+// Functional
+
